bdf1774d356e0e35e34cdd38c95741bd7f9e6f93
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-21 11:40:43 -0300

    Feat: Adição do Método a classe

--- index.php
+++ index.php
@@ -9,6 +9,25 @@
     <main>
         <h1>PHP com POO</h1>
         <hr>
+        <h2>Assuntos abordados</h2>
+        <ul>
+            <li>Crição de Classe</li>
+            <li>Importação do Arquivo de Classe</li>
+            <li>Criação de instâncias/Objetos</li>
+        </ul>
+        
+    <?php 
+        // importando a classe
+        require_once "src/Cliente.php";
+
+        // criar instâncias da Classe (objetos!)
+        $clienteA = new Cliente;
+        $clienteB = new Cliente;
+    ?>
+
+    <pre><?=var_dump($clienteA, $clienteB)?></pre>
     </main>
+
+    
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -2,10 +2,17 @@
 
 class Cliente{
     // Propriedades (ou atributos)
+    //public é o sinal de + do diagrama
 
     public string $nome;
     public string $email;
     public string $senha;
     public array $telefones;
+
+    // Métodos ou Comportamentos
+
+    public function exibir_dados() : void{
+        
+    }
 
 }
