3a9a0841649ac0a990a404f8e7c1e3e7572437f5
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-21 11:59:18 -0300

    Feat:  Acesso e Atribuição de Classe

--- index.php
+++ index.php
@@ -7,23 +7,41 @@
 </head>
 <body>
     <main>
-        <h1>PHP com POO</h1>
+        <h1>PHP com POO - Exemplo 2</h1>
         <hr>
         <h2>Assuntos abordados</h2>
         <ul>
-            <li>Crição de Classe</li>
-            <li>Importação do Arquivo de Classe</li>
-            <li>Criação de instâncias/Objetos</li>
+           <li>Acesso direto às Propriedades</li>
+           <li>Atribuição e Leitura de Dados</li>
+           <li>Chamda de método</li>
         </ul>
         
     <?php 
-        // importando a classe
         require_once "src/Cliente.php";
 
-        // criar instâncias da Classe (objetos!)
         $clienteA = new Cliente;
         $clienteB = new Cliente;
+
+        // Acesso e Atribuição
+        $clienteA->nome = "Tanaka";
+        $clienteA->telefones = ["(11)12345-6789", "(11)98765-4321"];
+        $clienteA->email = "tankeira3800@gmail.com";
+        $clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
+
+
+        $clienteB->nome = "ValeskaGPT";
+        $clienteB->telefones = ["(13)23456-7890", "(12)3456-7890"];
+        $clienteB->email = "meufilhochatGpt@gmail.com";
+        $clienteB->senha = password_hash("456def", PASSWORD_DEFAULT);
     ?>
+
+    <hr>
+
+    <h2>Dados dos Objetos: </h2>
+    <h3> Nome: <?= $clienteA->nome?> </h3>
+    <h3> Nome: <?= $clienteB->nome?> </h3>
+
+    <hr>
 
     <pre><?=var_dump($clienteA, $clienteB)?></pre>
     </main>
