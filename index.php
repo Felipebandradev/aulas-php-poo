99c3cb5d159a2c0b32ee855ca8784ee9d91e43b6
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-26 08:31:29 -0300

    Exemplo 04 encasulamento

--- index.php
+++ index.php
@@ -3,12 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo</title>
+    <title>Exemplo 4</title>
 </head>
 <body>
-    <main>
-        <h1>PHP com POO</h1>
-        <hr>
-    </main>
+    <h1>PHP com POO - Exemplo 4</h1>
+    <hr>
+    <h2>Assuntos abordados:</h2>
+    <ul>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibilidade 
+        (public, private e protected)</li>
+    </ul>
+
+<?php
+require_once "src/Cliente.php";
+$clienteA = new Cliente;
+
+/* O código abaixo dará erro pois
+a propriedade é privada! */
+//$clienteA->nome = "Tiago";
+
+/* Usando o setter para atribuir um valor 
+à propriedade privada "nome" */
+$clienteA->setNome("Fulano da Silva");
+
+
+$clienteA->setEmail("fulano@gmail.com");
+$clienteA->setSenha("123abc");
+?>
+
+<!-- Usando o getter para ler/acessar dados privados -->
+<p>Nome: <?=$clienteA->getNome()?></p>
+<p>E-mail: <?=$clienteA->getEmail()?></p>
+<p>Senha: <?=$clienteA->getSenha()?></p>
+
+<pre> <?=var_dump($clienteA)?> </pre>
+
+
 </body>
 </html>
---
+++ src/Cliente.php
@@ -0,0 +1,32 @@
+<?php
+class Cliente {
+
+    private string $nome;
+    private string $email; 
+    private string $senha;
+   
+    /* Métodos getters e setters */
+    public function setNome(string $nome):void {
+        $this->nome = $nome;
+    }
+
+    public function getNome():string {
+        return $this->nome;
+    }
+
+    public function setEmail(string $email):void {
+        $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    }
+
+    public function getEmail():string {
+        return $this->email;
+    }
+
+    public function setSenha(string $senha):void {
+        $this->senha = password_hash($senha, PASSWORD_DEFAULT);
+    }
+
+    public function getSenha():string {
+        return $this->senha;
+    }
+}
