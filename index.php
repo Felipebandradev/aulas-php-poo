f4b732665b6907f5298d9f9d08490ee02c466609
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-26 10:42:19 -0300

    Feat: Herança

--- index.php
+++ index.php
@@ -3,42 +3,115 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 4</title>
+    <title>Exemplo 05</title>
+
+    <style>
+
+        *{
+            box-sizing: border-box;
+        }
+
+        .caixa{
+            display: flex;
+        }
+        .cartao{
+            margin: 30px;
+            background-color: brown;
+            color: aliceblue;
+            padding: 10px;
+            width: 400px;
+            font-family:Arial, Helvetica, sans-serif;
+            border-radius: 20px;
+        }
+
+        .cartao img{
+            width: 140px;
+            height: 140px;
+            clip-path: circle(50% at 50% 49%);
+        }
+        
+
+        .cartao h2{
+            text-align: center;
+        }
+        .cartao ul{
+            text-align: center;
+            background-color: #ffc4b9;
+            padding: 20px;
+            color: #6d2721;
+            border-radius: 20px;
+            list-style: none;
+        }
+    </style>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 4</h1>
-    <hr>
-    <h2>Assuntos abordados:</h2>
-    <ul>
-        <li>Encapsulamento</li>
-        <li>Modificadores de visibilidade 
-        (public, private e protected)</li>
-    </ul>
+    <main>
+        <h1>PHP com POO - Exemplo 4</h1>
+        <hr>
+        <h2>Assuntos abordados:</h2>
+        <ul>
+            <li>Herança (Super classe e Sub Classe)</li>
+           
+        </ul>
 
-<?php
-require_once "src/Cliente.php";
-$clienteA = new Cliente;
+        <hr>
 
-/* O código abaixo dará erro pois
-a propriedade é privada! */
-//$clienteA->nome = "Tiago";
+        <h2>clientePF</h2>
 
-/* Usando o setter para atribuir um valor 
-à propriedade privada "nome" */
-$clienteA->setNome("Fulano da Silva");
+        <?php 
+          
+          require_once "src/PessoaFisica.php";
 
+          $clientePF = new PessoaFisica;
 
-$clienteA->setEmail("fulano@gmail.com");
-$clienteA->setSenha("123abc");
-?>
+          // colocar um nome 
 
-<!-- Usando o getter para ler/acessar dados privados -->
-<p>Nome: <?=$clienteA->getNome()?></p>
-<p>E-mail: <?=$clienteA->getEmail()?></p>
-<p>Senha: <?=$clienteA->getSenha()?></p>
+          $clientePF->setNome("Maria do Bairro");
+          $clientePF->setEmail("mariadb@sql.com");
+          $clientePF->setIdade(50);
+          $clientePF->setCpf("123.456.789-10");
+        
 
-<pre> <?=var_dump($clienteA)?> </pre>
+          require_once "src/PessoaJuridica.php";
 
+          $clientePJ = new PessoaJuridica;
 
+          $clientePJ->setAnoFundacao(2020);
+          $clientePJ->setCnpj("66.666.6666/666.66");
+          $clientePJ->setEmail("odeiomeusfas@souamaioral.com");
+          $clientePJ->setNome("Doja Cat");
+          $clientePJ->setNomeFantasia("Scarlet");
+        ?>
+
+        <pre><?=var_dump($clientePF)?></pre>
+
+        <hr>
+
+        <article class="caixa">
+            <section class="cartao">
+                <h2>clientePJ </h2>
+                <h2><img src="https://cdn2.tribunaonline.com.br/img/inline/150000/1200x720/Playlist-da-Semana-Doja-Cat-lanca-seu-novo-album-S0015065200202309221136-2.webp?fallback=https%3A%2F%2Fcdn2.tribunaonline.com.br%2Fimg%2Finline%2F150000%2FPlaylist-da-Semana-Doja-Cat-lanca-seu-novo-album-S0015065200202309221136.jpg%3Fxid%3D629160%26resize%3D1000%252C500%26t%3D1695393375&xid=629160" alt="scarlet persna de doja cat"></h2>
+                <ul>
+                    <li><b>Nome: </b><?=$clientePJ->getNome()?></li>
+                    <li><b>Email: </b><?=$clientePJ->getEmail()?></li>
+                    <li><b>Ano: </b><?=$clientePJ->getAnoFundacao()?></li>
+                    <li><b>Cnpj: </b><?=$clientePJ->getCnpj()?></li>
+                    <li><b>Nome Fantasia: </b><?=$clientePJ->getNomeFantasia()?></li>
+                </ul>
+            </section>
+            <section class="cartao">
+                <h2>clientePJ </h2>
+                <h2><img src="https://assets.papelpop.com/wp-content/uploads/2021/08/doja-cat.png" alt="scarlet persna de doja cat"></h2>
+                <ul>
+                    <li><b>Nome: </b><?=$clientePJ->getNome()?></li>
+                    <li><b>Email: </b><?=$clientePJ->getEmail()?></li>
+                    <li><b>Ano: </b><?=$clientePJ->getAnoFundacao()?></li>
+                    <li><b>Cnpj: </b><?=$clientePJ->getCnpj()?></li>
+                    <li><b>Nome Fantasia: </b><?=$clientePJ->getNomeFantasia()?></li>
+                </ul>
+            </section>
+        </article>
+
+    </main>
 </body>
 </html>
