b43b776ae668c8f9ffdecb5aad8a5d171e0761f0
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-26 11:43:31 -0300

    Feat: Classes Final

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 
         .caixa{
             display: flex;
+            align-items: center;
+            justify-content: space-around;
+
+            flex-flow: row wrap;
         }
         .cartao{
             margin: 30px;
@@ -42,18 +46,29 @@
             border-radius: 20px;
             list-style: none;
         }
+
+        .cartaoPf {
+     
+            background-color: #845ee6;
+           
+        }
+
+        .cartaoPf ul{
+            background-color: #d3bdf6;
+            color: #36236a;
+        }
     </style>
 </head>
 <body>
     <main>
-        <h1>PHP com POO - Exemplo 4</h1>
+        <h1>PHP com POO - Exemplo 5</h1>
         <hr>
         <h2>Assuntos abordados:</h2>
         <ul>
             <li>Herança (Super classe e Sub Classe)</li>
             <li>Métodos/propriedades protegidos (acessiveis apenas pela classe em que foram definidos pelas subclasses)</li>
-            <li>Classe Abstrata</li>
-            <li>Classe Final</li>
+            <li>Classe Abstrata: Não Permitir Intancias/Objetos, Serve apenas de modelo para subclasses</li>
+            <li>Classe Final: não permite herança, ou seja, não aceita criação de subclasses</li>
            
         </ul>
 
@@ -84,6 +99,15 @@
           $clientePJ->setEmail("odeiomeusfas@souamaioral.com");
           $clientePJ->setNome("Doja Cat");
           $clientePJ->setNomeFantasia("Scarlet");
+
+          require_once "src/Mei.php";
+
+          $clienteMei = new Mei;
+
+          $clienteMei->setNome("Jão");
+          $clienteMei->setNomeFantasia("Super");
+          $clienteMei->setAnoFundacao(2023);
+          $clienteMei->setAreaDeAtuacao("Musicas Nacionais");
         ?>
 
         <pre><?=var_dump($clientePF)?></pre>
@@ -91,6 +115,10 @@
         <h2>clientePJ</h2>
 
         <pre><?=var_dump($clientePJ)?></pre>
+
+        <h2>Cliente Mei</h2>
+
+        <pre><?=var_dump($clienteMei)?></pre>
 
         <hr>
 
@@ -106,7 +134,7 @@
                     <li><b>Nome Fantasia: </b><?=$clientePJ->getNomeFantasia()?></li>
                 </ul>
             </section>
-            <section class="cartao">
+            <section class="cartao cartaoPf">
                 <h2>clientePF </h2>
                 <h2><img src="https://assets.papelpop.com/wp-content/uploads/2021/08/doja-cat.png" alt=" doja cat"></h2>
                 <ul>
@@ -114,6 +142,16 @@
                     <li><b>Email: </b><?=$clientePF->getEmail()?></li>
                     <li><b>Idade: </b><?=$clientePF->getIdade()?></li>
                     <li><b>Cpf: </b><?=$clientePF->getCpf()?></li>
+                </ul>
+            </section>
+            <section class="cartao ">
+                <h2>clienteMei </h2>
+                <h2><img src="https://i.scdn.co/image/ab6761610000e5ebbadd159b5a79377deefa6336" alt=" doja cat"></h2>
+                <ul>
+                    <li><b>Nome: </b><?=$clienteMei->getNome()?></li>
+                    <li><b>Area de Atuação: </b><?=$clienteMei->getAreaDeAtuacao()?></li>
+                    <li><b>Ano: </b><?=$clienteMei->getAnoFundacao()?></li>
+                    <li><b>Nome Fantasia: </b><?=$clienteMei->getNomeFantasia()?></li>
                 </ul>
             </section>
         </article>
---
+++ src/Mei.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "PessoaJuridica.php";
+
+final class Mei extends PessoaJuridica{
+
+    private string $areaDeAtuacao;
+
+    
+    public function getAreaDeAtuacao(): string
+    {
+        return $this->areaDeAtuacao;
+    }
+
+    public function setAreaDeAtuacao(string $areaDeAtuacao): self
+    {
+        $this->areaDeAtuacao = $areaDeAtuacao;
+
+        return $this;
+    }
+
+}
