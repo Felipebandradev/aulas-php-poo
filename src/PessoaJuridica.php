252e785977a54539c8de57612e856cf9ef0822d6
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-28 11:09:13 -0300

    Implementação de exibir dados na classe pessoa jurídica

--- src/PessoaJuridica.php
+++ src/PessoaJuridica.php
@@ -13,6 +13,17 @@ class PessoaJuridica extends Cliente{
         // acessando o metodo setter protegido
         $this->setSituacao("em analise");
     }
+
+    public function exibirDados(): void
+    {
+        echo "<h3>Método Exibir Dados - Classe Pessoa Juridica - Polimorfismo de sobreposição</h3>";
+        // Ophp só aceita esse tipo
+        //  echo "<h4> $this->nome </h4>"; essa linha não ira funcionar pois o atributo nome é privado e da classe cliente
+          echo "<h4>".$this->getNome()."</h4>";
+  
+          echo "<p>Situação:". $this->getSituacao()." </p>";
+          echo "<p>Noem Fantasia: $this->nomeFantasia </p>";
+    }
    
     public function getAnoFundacao(): int
     {
