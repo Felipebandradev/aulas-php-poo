55189fdde9c0d5a2dd1eb38b15693d6f35151dd7
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-09-29 10:32:12 -0300

    Pequenos Ajustes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 09</title>
+    <title>Exemplo 10</title>
 
     <style>
         body {
@@ -67,18 +67,16 @@
     <main>
 
         <body>
-            <h1>PHP com POO - Exemplo 9</h1>
+            <h1>PHP com POO - Exemplo 10</h1>
             <hr>
             <h2>Assuntos abordados:</h2>
             <ul>
-                <li>Namespaces: agrupamento e organização de recursos (classes, funções, constantes)</li>
-                <li>Prevenção de conflitos entre classes de mesmo nome</li>
-                <li>Configurar e usar <code>namespaces</code> e <code>alias</code></li>
+                <li>Composer: gerenciador de dependência PHP</li>
+                <li>Configurar o projeto usando <code>composer.json</code></li>
+                <li>Autoload de classes evitando muitos requires e/ou require manuais</li>
             </ul>
 
-            <?php
-            /* Só de fazer o require/importação das classes (SEM NAMESPACE),
-    já dá erro no servidor devido a terem o mesmo nome. */
+            <?php           
 
             use Fornecedor\Pagamento;
             use Prestador\Pagamento as PrestadorPagamento;
@@ -88,21 +86,9 @@
 
             require_once "src/fornecedores/Pagamento.php";
             require_once "src/prestadores/Pagamento.php";
-
-            // Forma 1 de usar classe com namespace 
-            // $pagamentoFornecedor = new Fornecedor\Pagamento;
-            // $pagamentoPrestador = new Prestador\Pagamento;
-
-            // Forma 2 de usar classe com namespace 
-            // ela é a mais utilizada
-
-            // use Fornecedor\Pagamento;
-            // use Prestador\Pagamento as PrestadorPagamento;
-
-            // $pagamentoFornecedor = new Pagamento;
-            // $pagamentoPrestador = new PrestadorPagamento; // objeto através de Alias/apelido
-
-            // Forma 3 de usar classe com namespace 
+            require_once "src/PessoaFisica.php";
+            require_once "src/PessoaJuridica.php";
+            require_once "src/Mei.php";
 
             $pagamentoFornecedor = new Pagamento;
 
@@ -115,16 +101,9 @@
 
             <hr>
 
-            <!-- 
-                - Crie objetos ClienteMei, ClientePF, ClientePJ
-                - Coloque alguns dados usando setters
-                - Exiba alguns dados no HTML
-            -->
 
             <?php
-            require_once "src/PessoaFisica.php";
-            require_once "src/PessoaJuridica.php";
-            require_once "src/Mei.php";
+
 
             $clienteMei = new Mei;
 
